2df402972e77ca135d35ac33051cc60ae8003674
eronides neto <eron93@gmail.com>
2018-07-02 18:24:17 -0300

    Update test-DHT.ino

--- workshop-iot/test-DHT.ino
+++ workshop-iot/test-DHT.ino
@@ -2,26 +2,30 @@
  * WORKSHOP INTRODUCAO A IOT @SENAI -- 19/07/2018
  * Código para testar o sensor DHT11
 */
+#include "DHT.h"
 
+float temperatura =0;
+float umidade=0;
 
-#include <dht.h>
+DHT dht(D4,DHT11);
 
-dht DHT;
-
-#define DHT11_PIN 2
-
-void setup()
+void setup() 
 {
   Serial.begin(9600);
+  dht.begin(); 
+  delay(2000);
 }
 
-void loop()
+void loop() 
 {
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperatura = ");    // imprime no serial monitor o valor da temperatura
-  Serial.println(DHT.temperature);
-  Serial.print("Umididade = ");
-  Serial.println(DHT.humidity);      // imprime no serial monitor o valor da umidade
-  delay(1000);                       // insere um atraso de 1s
+  // lo - Aquisicao do sinais de sensores
+  umidade = (int) dht.readHumidity();
+  temperatura = (int)dht.readTemperature(); 
+  Serial.print("Umidade: ");
+  Serial.print(umidade);
+  Serial.print("  ");
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" *C");
+  delay(2000);
 }
-
